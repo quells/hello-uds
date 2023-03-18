0e344a5c3c9181f8657026daf2ab0f1181f020e3
Kai Wells <support@kaiwells.me>
2023-03-18 12:27:05 -0700

    hello uds client

--- cmd/uds-client/main.go
+++ cmd/uds-client/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"github.com/quells/hello-uds/internal/env"
+	"log"
+	"net"
+)
 
+func main() {
+	addr, err := net.ResolveUnixAddr("unix", env.Config.SocketFile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	conn, err := net.DialUnix("unix", nil, addr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer func() {
+		if cErr := conn.Close(); cErr != nil {
+			log.Println(cErr)
+		}
+	}()
+
+	_, err = conn.Write([]byte("hello"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("got %d bytes: %v", n, buf[:n])
 }
